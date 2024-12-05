74ab5b6ced4bab5bcd0163c42091240aaf1809df
unknown <asadalgalib9@gmail.com>
2024-12-06 01:16:45 +0600

    database conntected and env variable added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,10 +10,29 @@ app.use(express.json());
 app.use(cors());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bidfx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
+async function run() {
+  try {
 
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
+  } catch {
+    // await client.close();
+    console.log(error);
+  }
+}
+run();
 
 
 
